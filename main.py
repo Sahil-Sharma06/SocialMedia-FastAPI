9c05319391f899da83f9d175634be5a8f7e6b2ab
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-10-26 03:49:05 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title:str
+    description:str
+    published:bool = True
+    rating: Optional[int] = None
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI"}
+
+
+@app.get("/posts")
+async def posts():
+    return {"data" : "These are your posts..."}
+
+
+@app.post("/createposts")
+async def create_post(new_post: Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data" : new_post}
